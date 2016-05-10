17beffc32ad8861b066e69a93d1364469aead0f3
Hausmans <HausmanS6691@sduhsd.lan>
2016-05-10 15:02:24 -0700

    Forgot to add Transmit class

--- CipherProject/src/Transmit.java
+++ CipherProject/src/Transmit.java
@@ -1,28 +1,44 @@
 import java.math.BigInteger;
+import java.util.Random;
 
 public class Transmit implements RSA {
+	
+	private BigInteger p;
+	private BigInteger q;
+	private BigInteger totient;
+	
+	public BigInteger n;
+	public BigInteger publicKey = new BigInteger("65537");
+	
+	public Transmit(){
+		publicKey();
+	}
 
 	public BigInteger stringConversion(String input) {
 		String text = input;
 		String number = "";
 		while (text.length() > 0) {
-			if (text.charAt(0) != 32)
-				number += text.charAt(0) - 47;
-			else
-				number += 0;
-			number += " ";
+				number += text.charAt(0) - 22;
 			text.substring(1);
 		}
 		return new BigInteger(number);
 	}
 
-	public BigInteger encrypt(String input) {
-		BigInteger number = stringConversion(input);
+	private BigInteger publicKey() {
+		p = BigInteger.probablePrime(10000, new Random());
+		q = BigInteger.probablePrime(10000, new Random());
+		n = p.multiply(q);
+		totient = (p.subtract(BigInteger.ONE)).multiply(q.subtract(BigInteger.ONE));
 		return null;
 	}
 
-	public BigInteger decrypt(BigInteger input) {
+	public String decrypt(BigInteger input, BigInteger n) {
 		return null;
+	}
+
+	public BigInteger encrypt(String input) {
+		BigInteger number = stringConversion(input);
+		return number.modPow(publicKey, n);
 	}
 
 }
